090540a5bc55d8c17c725797c2a52bea8dc5eb72
Sebastian <srcolinas@gmail.com>
2019-11-11 21:21:37 -0500

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="homework",
-    version="0.0.3",
+    version="0.0.4",
     author="Sebastian Rodriguez Colina",
     author_email="srcolinas@gmail.com",
     description="A package to create programming assignments",
