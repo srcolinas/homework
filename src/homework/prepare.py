4084b7be3e48cf128aa596faed5d75e4371b6b41
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2025-01-26 09:29:45 -0500

    remove encription

--- src/homework/prepare.py
+++ src/homework/prepare.py
@@ -1,15 +1,14 @@
 import pathlib
 import re
-from typing import Callable
 
 from . import parser
 
 
 def prepare(
-    source_dir: pathlib.Path, pattern: str, cipher: Callable[[str], str] = lambda x: x
-) -> tuple[pathlib.Path, pathlib.Path]:
-    homework_dir = _create_derived_directory(source_dir, "_homework")
-    solution_dir = _create_derived_directory(source_dir, "_solution")
+    source_dir: pathlib.Path, pattern: str
+) -> pathlib.Path:
+    homework_dir = source_dir.with_name(source_dir.stem + "_homework")
+    homework_dir.mkdir(parents=False)
 
     expresion = re.compile(pattern)
 
@@ -27,15 +26,5 @@ def prepare(
         homework = homework_dir / relative_path
         homework.parent.mkdir(exist_ok=True)
         homework.write_text(student_content)
+    return homework_dir
 
-        teacher_content = cipher(source_content)
-        solution = solution_dir / relative_path
-        solution.parent.mkdir(exist_ok=True)
-        solution.write_text(teacher_content)
-    return homework_dir, solution_dir
-
-
-def _create_derived_directory(source_dir: pathlib.Path, suffix: str) -> pathlib.Path:
-    dir = source_dir.with_name(source_dir.stem + suffix)
-    dir.mkdir(parents=False)
-    return dir
--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -12,36 +12,13 @@ dw = compute_gradients()
 w = w - alpha * dw
 ## homework:replace:off
 """)
-    homework_dir, _ = prepare.prepare(tmp_path, r".*source\.py$")
+    homework_dir = prepare.prepare(tmp_path, r".*source\.py$")
     assert (
         (homework_dir / "source.py").read_text()
         == """## homework:start
 dw =
 w =
 ## homework:end"""
-    )
-
-
-
-
-def test_produces_encripted_source(tmp_path: pathlib.Path):
-    (tmp_path / "source.py").write_text("""## homework:replace:on
-#.dw =
-#.w =
-dw = compute_gradients()
-w = w - alpha * dw
-## homework:replace:off
-""")
-    _, solution_dir = prepare.prepare(tmp_path, r".*source\.py$", cipher=lambda x: x.upper())
-    assert (
-        (solution_dir / "source.py").read_text()
-        == """## HOMEWORK:REPLACE:ON
-#.DW =
-#.W =
-DW = COMPUTE_GRADIENTS()
-W = W - ALPHA * DW
-## HOMEWORK:REPLACE:OFF
-"""
     )
 
 
@@ -53,15 +30,10 @@ def test_handles_pattern_from_source_dir(tmp_path: pathlib.Path):
     (subdir / "module.py").touch()
     (subdir / "data.json").touch()
 
-    homework_dir, solution_dir = prepare.prepare(tmp_path, r".*\.(py|json)$")
+    homework_dir = prepare.prepare(tmp_path, r".*\.(py|json)$")
     assert (homework_dir / "main.py").exists()
     assert (homework_dir / "subdir" / "module.py").exists()
     assert (homework_dir / "subdir" / "data.json").exists()
     assert not (homework_dir / "first.rs").exists()
-
-    assert (solution_dir / "main.py").exists()
-    assert (solution_dir / "subdir" / "module.py").exists()
-    assert (solution_dir / "subdir" / "data.json").exists()
-    assert not (solution_dir / "first.rs").exists()
 
 
