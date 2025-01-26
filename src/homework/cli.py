5b15e35675cab65f673ee257c03aa39d3ccc4022
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2025-01-26 09:30:06 -0500

    add cli

---
+++ src/homework/cli.py
@@ -0,0 +1,15 @@
+import argparse
+import pathlib
+
+from . import prepare
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source_dir", type=pathlib.Path)
+    parser.add_argument("-p", "--pattern", default=".*")
+    args = parser.parse_args()
+
+
+    homework_dir = prepare.prepare(args.source_dir, pattern=args.pattern)
+    print(f"Homework created from {args.source_dir} in {homework_dir}")
